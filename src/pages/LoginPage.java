206f7a860c7989c9654a826d8a30b679d18ba8da
dileepsagarreddy <dileep0399@gmail.com>
2025-11-15 12:52:16 +0530

    added/updated/modified

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -3,6 +3,9 @@ package pages;
 public class LoginPage {
 	public static void main(String arg[]) {
 		System.out.println("Hello LP");
+		String un="admin";
+		String pwd="test123";
+		System.out.println("Logged in");
 	}
 
 }
---
+++ src/pages/searchPage.java
@@ -0,0 +1,8 @@
+package pages;
+
+public class searchPage {
+	
+	public static void main(String arg[]) {
+		System.out.println("Hello SP");
+	}
+}
