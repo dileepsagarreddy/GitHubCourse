c1c86e9eb6144780df0804cdbc3d1ceb4fb1915f
dileepsagarreddy <dileep0399@gmail.com>
2025-11-30 11:33:02 +0530

    forth cherry commit

--- src/pages/CherryPage.java
+++ src/pages/CherryPage.java
@@ -1,15 +1,20 @@
-	package pages;
+package pages;
 
 public class CherryPage {
-	
+
 	private void sysout() {
-	
+
 		String name ="Naveen";
 
 		int salary = 100;
-	//	bcd7e34 (HEAD -> master) first cherry commit
+		//	bcd7e34 (HEAD -> master) first cherry commit
 
-	//	fef49e8 (HEAD -> master) second charry commit
+		//	fef49e8 (HEAD -> master) second charry commit
+		
+
+	}
+
+	public void test() {
 
 	}
 
