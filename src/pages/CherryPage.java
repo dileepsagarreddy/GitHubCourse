31f78da077821d3fecf73b01c2944ef99352f063
dileepsagarreddy <dileep0399@gmail.com>
2025-11-30 11:19:39 +0530

    removed conflits

--- src/pages/CherryPage.java
+++ src/pages/CherryPage.java
@@ -5,7 +5,12 @@ public class CherryPage {
 	private void sysout() {
 	
 		String name ="Naveen";
-		
+
+		int salary = 100;
+	//	bcd7e34 (HEAD -> master) first cherry commit
+
+	//	fef49e8 (HEAD -> master) second charry commit
+
 	}
 
 }
