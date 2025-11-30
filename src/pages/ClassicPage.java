c3d4a78eda873acbf8c4edffc112186c3caffe38
dileepsagarreddy <dileep0399@gmail.com>
2025-11-30 18:19:06 +0530

    modified the file

--- src/pages/ClassicPage.java
+++ src/pages/ClassicPage.java
@@ -1,11 +1,13 @@
 package pages;
 
 public class ClassicPage {
-public static void classic() {
-	int k=100;
-	int k1=20;
-	int div=k1/k;
-	System.out.println(div);
-  System.out.println("BYE");
-}
+	public static void classic() {
+		int k=100;
+		int k1=20;
+		int div=k1/k;
+		System.out.println(div);
+		System.out.println("BYE");
+
+		String name="Naveen";
+	}
 }
