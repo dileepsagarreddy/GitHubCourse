4606060e4d309bb51568a7b3af55670313ec4ff4
Dileep <40707204+dileepsagarreddy@users.noreply.github.com>
2025-11-30 17:57:32 +0530

    Create ClassicPage.java

---
+++ src/pages/ClassicPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class ClassicPage {
+public static void classic() {
+	int k=100;
+	int k1=20;
+	int div=k1/k;
+	System.out.println(div);
+  System.out.println("BYE");
+}
+}
